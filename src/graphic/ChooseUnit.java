5b4e7ce83cd0168f0cc9324b05bee8c92c128705
Mahan Zendedel <43912482+Mahanmmi@users.noreply.github.com>
2019-05-18 20:04:50 +0430

    ChooseUnit Code Cleaned!

--- src/graphic/ChooseUnit.java
+++ src/graphic/ChooseUnit.java
@@ -7,11 +7,12 @@ import javax.swing.*;
 import static graphic.GUI.chessBoard;
 import static graphic.GUI.mainFrame;
 
-public class ChooseUnit extends JButton {
+class ChooseUnit extends JButton {
     private Coordinate coordinate;
     private String textt;
     private JFrame chooseWindow;
-    public ChooseUnit(String text, Coordinate coordinate, JFrame chooseWindow) {
+
+    ChooseUnit(String text, Coordinate coordinate, JFrame chooseWindow) {
         super(text);
         this.chooseWindow = chooseWindow;
         this.textt = text;
@@ -19,21 +20,27 @@ public class ChooseUnit extends JButton {
         addListener();
     }
 
-    private void addListener(){
-        addMouseListener(new java.awt.event.MouseAdapter(){
-            public void mouseClicked(java.awt.event.MouseEvent evt){
+    private void addListener() {
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
                 AbstractPiece newAbstractPiece = null;
-                if(textt.equals("Bishop")){
-                    newAbstractPiece = new Bishop(chessBoard[coordinate.getX()][coordinate.getY()].getAbstractPiece().isWhite());
-                }
-                else if(textt.equals("Knight")){
-                    newAbstractPiece = new Knight(chessBoard[coordinate.getX()][coordinate.getY()].getAbstractPiece().isWhite());
-                }
-                else if(textt.equals("Rook")){
-                    newAbstractPiece = new Rook(chessBoard[coordinate.getX()][coordinate.getY()].getAbstractPiece().isWhite());
-                }
-                else if(textt.equals("Queen")){
-                    newAbstractPiece = new Queen(chessBoard[coordinate.getX()][coordinate.getY()].getAbstractPiece().isWhite());
+                switch (textt) {
+                    case "Bishop": {
+                        newAbstractPiece = new Bishop(chessBoard[coordinate.getX()][coordinate.getY()].getAbstractPiece().isWhite());
+                        break;
+                    }
+                    case "Knight": {
+                        newAbstractPiece = new Knight(chessBoard[coordinate.getX()][coordinate.getY()].getAbstractPiece().isWhite());
+                        break;
+                    }
+                    case "Rook": {
+                        newAbstractPiece = new Rook(chessBoard[coordinate.getX()][coordinate.getY()].getAbstractPiece().isWhite());
+                        break;
+                    }
+                    case "Queen": {
+                        newAbstractPiece = new Queen(chessBoard[coordinate.getX()][coordinate.getY()].getAbstractPiece().isWhite());
+                        break;
+                    }
                 }
                 chessBoard[coordinate.getX()][coordinate.getY()].setAbstractPiece(newAbstractPiece);
                 chessBoard[coordinate.getX()][coordinate.getY()].setIcon(newAbstractPiece.getIcon());
